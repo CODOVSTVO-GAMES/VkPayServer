64d09b0d9d16e683d5813f755b9f0c3dbc329c54
Keshox <keshoxvol@gmail.com>
2022-07-19 13:16:30 +0300

    test

--- src/main/java/ru/codovstvo/srvadmin/controllers/StatController.java
+++ src/main/java/ru/codovstvo/srvadmin/controllers/StatController.java
@@ -1,7 +1,10 @@
 package ru.codovstvo.srvadmin.controllers;
 
+import java.util.Arrays;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
+import java.util.Set;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -24,9 +27,29 @@ public class StatController {
     
     @GetMapping("funnel")
     public Map getFunnelStartEvents(@RequestParam(name = "version", required=false, defaultValue="") String version){
+        Set eventsName =  new HashSet<String>(Arrays.asList("first_load","started_game",
+                                                                "dialogue_marya_close_0","merge_marya",
+                                                                "dialogue_marya_close_1","merge_stebel",
+                                                                "dialogue_marya_close_2","merge_bloom_tree",
+                                                                "dialogue_marya_close_3","quest_done_0",
+                                                                "level_up_2","quest_open_1",
+                                                                "2_apple_harvested","first_click_in_marya",
+                                                                "click_exchange","click_speedup",
+                                                                "quest_done_1","quest_open_2",
+                                                                "open_map_2","first_click_in_root",
+                                                                "dialogue_cat_close_1","dialogue_marya_close_6",
+                                                                "quest_done_2","level_up_3",
+                                                                "dialogue_cat_close_2","second_click_in_root",
+                                                                "second_click_in_root","dialogue_cat_close_4",
+                                                                "first_click_in_chest","first_click_in_chest_open",
+                                                                "quest_open_6","dialogue_cat_close_5")); 
+
         Map responce = new HashMap<>();
         if (version.equals("")){
-            responce.put("first_load", eventRepo.countByEventName("first_load"));
+            for(Object object : eventsName){
+                String event = (String) object;
+                responce.put(event, eventRepo.countByEventName(event));
+            }
         }
 
         return responce;
