53d924112a6652416029e3c0e96a55376dd9b8a9
keshoxvol <51890346+keshoxvol@users.noreply.github.com>
2022-08-12 18:20:15 +0300

    Test addAppEvent

--- src/main/java/ru/codovstvo/srvadmin/services/SecureVkApiService.java
+++ src/main/java/ru/codovstvo/srvadmin/services/SecureVkApiService.java
@@ -2,7 +2,26 @@ package ru.codovstvo.srvadmin.services;
 
 import org.springframework.stereotype.Service;
 
+import com.vk.api.sdk.client.VkApiClient;
+import com.vk.api.sdk.client.actors.ServiceActor;
+import com.vk.api.sdk.httpclient.HttpTransportClient;
+import com.vk.api.sdk.objects.ServiceClientCredentialsFlowResponse;
+import com.vk.api.sdk.objects.apps.responses.GetResponse;
+import com.vk.api.sdk.objects.base.responses.OkResponse;
+
 @Service
 public class SecureVkApiService {
     
+    public void test() throws Exception {
+        
+        HttpTransportClient httpClient = new HttpTransportClient();
+        VkApiClient vk = new VkApiClient(httpClient);
+
+
+        ServiceClientCredentialsFlowResponse authResponse = vk.oAuth().serviceClientCredentialsFlow(8180902,"3diesn7ViJiMazGJa7my").execute();
+
+        ServiceActor actor = new ServiceActor(8180902, authResponse.getAccessToken());
+
+        OkResponse okResponse = vk.secure().addAppEvent(actor, 77517618, 1).value(2) .execute();
+    }
 }
