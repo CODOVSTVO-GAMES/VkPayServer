11bf2ca16cde10d04120cde460bfd7cd6446208e
keshoxvol <51890346+keshoxvol@users.noreply.github.com>
2022-08-15 14:27:56 +0300

    test keys

--- src/main/java/ru/codovstvo/srvadmin/services/SecureVkApiService.java
+++ src/main/java/ru/codovstvo/srvadmin/services/SecureVkApiService.java
@@ -8,6 +8,7 @@ import com.vk.api.sdk.httpclient.HttpTransportClient;
 import com.vk.api.sdk.objects.ServiceClientCredentialsFlowResponse;
 import com.vk.api.sdk.objects.base.responses.OkResponse;
 import com.vk.api.sdk.objects.secure.responses.CheckTokenResponse;
+import com.vk.api.sdk.streaming.clients.VkStreamingApiClient;
 
 @Service
 public class SecureVkApiService {
@@ -15,11 +16,12 @@ public class SecureVkApiService {
     public void test() throws Exception {
         
         HttpTransportClient httpClient = new HttpTransportClient();
+        
         VkApiClient vk = new VkApiClient(httpClient);
 
         ServiceClientCredentialsFlowResponse authResponse = vk.oAuth().serviceClientCredentialsFlow(8180902,"3diesn7ViJiMazGJa7my").execute();
 
-        ServiceActor actor = new ServiceActor(8180902, "3diesn7ViJiMazGJa7my", authResponse.getAccessToken());
+        ServiceActor actor = new ServiceActor(8180902, "3diesn7ViJiMazGJa7my", "c7c21837c7c21837c7c21837fbc7becc91cc7c2c7c21837a534417df3e7eab26d5a63cf");
         System.out.println(actor.getAccessToken());
 
         // OkResponse okResponse = vk.secure().addAppEvent(actor, 77517618, 1).execute();
