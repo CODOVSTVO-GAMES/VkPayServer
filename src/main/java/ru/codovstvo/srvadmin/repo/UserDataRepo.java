11a2ee0c2f29a0cc1bdb7469acc902f6a844da4e
keshoxvol <51890346+keshoxvol@users.noreply.github.com>
2022-10-06 10:39:34 +0300

    test

--- src/main/java/ru/codovstvo/srvadmin/repo/UserDataRepo.java
+++ src/main/java/ru/codovstvo/srvadmin/repo/UserDataRepo.java
@@ -3,9 +3,6 @@ package ru.codovstvo.srvadmin.repo;
 import org.springframework.data.repository.CrudRepository;
 
 import ru.codovstvo.srvadmin.entitys.UserData;
-import ru.codovstvo.srvadmin.entitys.UserEntity;
 
 public interface UserDataRepo extends CrudRepository<UserData, Long> {
-    UserData findByUserAndTitle(UserEntity user, String title);
-    int countByUserIdAndTitle(int userId, String title);
 }
