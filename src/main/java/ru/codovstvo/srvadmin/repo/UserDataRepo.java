18c2e5a8ee85f09a91a4b3c80de20e6a22130039
keshoxvol <51890346+keshoxvol@users.noreply.github.com>
2022-10-06 15:36:56 +0300

    ddd

--- src/main/java/ru/codovstvo/srvadmin/repo/UserDataRepo.java
+++ src/main/java/ru/codovstvo/srvadmin/repo/UserDataRepo.java
@@ -5,8 +5,9 @@ import java.util.Set;
 import org.springframework.data.repository.CrudRepository;
 
 import ru.codovstvo.srvadmin.entitys.UserData;
+import ru.codovstvo.srvadmin.entitys.UserEntity;
 
 public interface UserDataRepo extends CrudRepository<UserData, Long> {
-    Set<UserData> findByUser(Long user);
-    void deleteByUser(Long user);
+    Set<UserData> findByUser(UserEntity user);
+    void deleteByUser(UserEntity user);
 }
