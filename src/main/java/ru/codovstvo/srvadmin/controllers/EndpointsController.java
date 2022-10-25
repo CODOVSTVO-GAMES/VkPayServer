a5ed6a47d00c5adcb385353f5bbfc7bdb8304ff0
keshoxvol <51890346+keshoxvol@users.noreply.github.com>
2022-10-25 11:44:20 +0300

    data controller

--- src/main/java/ru/codovstvo/srvadmin/controllers/EndpointsController.java
+++ src/main/java/ru/codovstvo/srvadmin/controllers/EndpointsController.java
@@ -5,9 +5,13 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import ru.codovstvo.srvadmin.entitys.EventEntity;
 import ru.codovstvo.srvadmin.entitys.UserEntity;
+import ru.codovstvo.srvadmin.entitys.Version;
+import ru.codovstvo.srvadmin.repo.EventRepo;
 import ru.codovstvo.srvadmin.repo.SessionsRepo;
 import ru.codovstvo.srvadmin.repo.UserEntityRepo;
+import ru.codovstvo.srvadmin.repo.VersionRepo;
 
 import org.springframework.transaction.annotation.Transactional;
 
@@ -22,6 +26,12 @@ public class EndpointsController {
     @Autowired
     SessionsRepo sessionsRepo;
     
+    @Autowired
+    EventRepo eventRepo;
+
+    @Autowired
+    VersionRepo versionRepo;
+    
     @GetMapping(value="/allusers")
     public Iterable<UserEntity> getUsers() {
         Iterable<UserEntity> users = userEntityRepo.findAll();
@@ -29,15 +39,15 @@ public class EndpointsController {
     }
 
     @GetMapping(value="/allevents")
-    public Iterable<UserEntity> getEvents() {
-        Iterable<UserEntity> users = userEntityRepo.findAll();
-        return users;
+    public Iterable<EventEntity> getEvents() {
+        Iterable<EventEntity> events = eventRepo.findAll();
+        return events;
     }
 
     @GetMapping(value="/allversions")
-    public Iterable<UserEntity> getVersions() {
-        Iterable<UserEntity> users = userEntityRepo.findAll();
-        return users;
+    public Iterable<Version> getVersions() {
+        Iterable<Version> versions = versionRepo.findAll();
+        return versions;
     }
     
 }
