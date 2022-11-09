ef698e96d645cd2fd20b81b41ea9e7a497e5bf70
keshoxvol <51890346+keshoxvol@users.noreply.github.com>
2022-11-09 11:12:41 +0300

    r

--- src/main/java/ru/codovstvo/srvadmin/services/AutoService.java
+++ src/main/java/ru/codovstvo/srvadmin/services/AutoService.java
@@ -91,12 +91,12 @@ public class AutoService {
                     
                     try{
                         secureVkApiService.sendNotification(unit.getUserEntity().getPlatformUserId(), not);
-                        try {
-                            Thread.sleep(1 * 1000);
-                        } catch (InterruptedException ie) {
-                            Thread.currentThread().interrupt();
-                        }
                     }catch (Exception e){System.out.println("Сообщение крашнулось");}
+                    try {
+                        Thread.sleep(1 * 1000);
+                    } catch (InterruptedException ie) {
+                        Thread.currentThread().interrupt();
+                    }
 
                     unit.getUserEntity().setLastNotification(not);
                     userEntityRepo.save(unit.getUserEntity());
