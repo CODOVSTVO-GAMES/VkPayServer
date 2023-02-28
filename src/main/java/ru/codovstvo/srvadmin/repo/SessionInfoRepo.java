09963eec6bce469b761a753220e266d92464a385
keshoxvol <51890346+keshoxvol@users.noreply.github.com>
2023-02-28 13:14:37 +0300

    sunc

--- src/main/java/ru/codovstvo/srvadmin/repo/SessionInfoRepo.java
+++
@@ -1,8 +0,0 @@
-package ru.codovstvo.srvadmin.repo;
-
-import org.springframework.data.repository.CrudRepository;
-
-import ru.codovstvo.srvadmin.entitys.SessionInfo;
-
-public interface SessionInfoRepo extends CrudRepository<SessionInfo, Long> {
-}
